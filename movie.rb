69a664978b56c43f8ce768a8b4d90ba3354225ae
han.jing <han.jing@rea-group.com>
2017-06-03 19:33:53 +0800

    Add mysql

--- movie.rb
+++ movie.rb
@@ -1,4 +1,15 @@
 require 'active_record'
+require 'mysql2'
+
+ActiveRecord::Base.establish_connection ({
+    :adapter => "mysql2",
+    :host => "127.0.0.1",
+    :username => "root",
+    :password => "root",
+    :database => "metaruby"
+})
+
+
 
 class Movie < ActiveRecord::Base
 
--- run.rb
+++ run.rb
@@ -1,8 +1,10 @@
 require_relative 'movie'
 
 movie = Movie.create
-movie.titleSet "Speed and Furious"
-movie.directorSet "Rob Cohen"
+movie.title= "Speed and Furious"
+movie.director= "Rob Cohen"  #didn't save the value in the database
 
 puts movie.title
 puts movie.director
+
+movie2 = Movie.create(:title => "aaa", :director => "bbb")
